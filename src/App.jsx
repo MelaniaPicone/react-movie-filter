76b3757440f19e02521138dcb3be06cfe33f11c1
MelaniaPicone <melania_picone@yahoo.it>
2025-09-08 13:54:28 +0200

    [feat] movie list return

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+const startMovies =  [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ];
+
 
 function App() {
-  <>
+  return (<>
+ <div className="container my-5">
+  <div className="row gy-4">
+    <div className="col-12">
+      <h1>React Movies</h1>
+    </div>
+    <div className="col-12">
+      <ul className="list-group ms-0">
+        {startMovies.map((movie, index)=> {
+          return (<li key={index} className="list-group-item">
+            {movie.title}
+          </li>);
+        })}
+      </ul>
+    </div>
+  </div>
+ </div>
+
+
   
-  </>
+  </> )
 }
 
 export default App
